4489a150d6128dfd7ca5631ac6eab7c68680548b
s-ferrari <sol_ferrari20@hotmail.com>
2025-09-03 18:03:48 -0300

    refactor: rename variables and improve test structure in DnD

--- 1 - dnd/D&D (very easy).js
+++ 1 - dnd/D&D (very easy).js
@@ -1,41 +1,25 @@
 const body = document.querySelector("body")
-function mostrar_texto(texto) {
-    const element = document.createElement("div")
-    element.textContent = texto
-    body.appendChild(element)
+
+function renderProbability(text) {
+    const element = document.createElement("div");
+    element.textContent = text;
+    body.appendChild(element);
 }
 
-// function probabilidad (valor) {
-//     if (valor == 20)
-//         return "exito critico"
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
 
-//     if (valor < 10)
-//         return "fallo"
+let failCount = 0
+let successCount = 0
+let modifier = 1
 
-//     if (valor >= 10)
-//         return "exito"
-// }
-
-let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
-
-let cantidad_de_fallos = 0
-let cantidad_de_exitos = 0
-let modificador = 1
-for (let numero of numeros) {
-    if (numero + modificador < 10)
-        cantidad_de_fallos++
-    else
-        cantidad_de_exitos++
+for (let number of numbers) {
+    if (number + modifier < 10) {
+        failCount++;
+    } else {
+        successCount++;
+    }
 }
-mostrar_texto("Fallos: " + cantidad_de_fallos * 5 + "%")
-mostrar_texto("Exitos: " + cantidad_de_exitos * 5 + "%")
-mostrar_texto("Exito critico: 5%")
 
-// let cantidad_de_fallos = 0
-// let cantidad_de_exitos = 0
-// for (let i = 1; i <= 19; i++) {
-//     if (i < 10)
-//         cantidad_de_fallos++
-//     else
-//         cantidad_de_exitos++
-// }
+renderProbability("Failures: " + failCount * 5 + "%");
+renderProbability("Successes: " + successCount * 5 + "%");
+renderProbability("Critical success: 5%");
