266e36f7c807c61bdd0cc075760d0c997e6bd3b4
s-ferrari <sol_ferrari20@hotmail.com>
2025-09-03 19:15:22 -0300

    refactor(Zweihander): rename variables and improve test structure

--- 2 - zweihander/Zweihander (easy).js
+++ 2 - zweihander/Zweihander (easy).js
@@ -1,27 +1,29 @@
 const body = document.querySelector("body")
-function mostrar_texto(texto) {
-    const element = document.createElement("div")
-    element.textContent = texto
-    body.appendChild(element)
+
+function renderProbability(text) {
+    const element = document.createElement("div");
+    element.textContent = text;
+    body.appendChild(element);
 }
 
-let cantidad_fallos = 0
-let cantidad_exitos = 0
-let cantidad_exito_critico = 0
-let cantidad_fallo_critico = 0
+let failCount = 0;
+let successCount = 0;
+let criticalSuccessCount = 0;
+let criticalFailCount = 0;
 
 for (let i = 1; i <= 100; i++) {
-    if (i == 1 || (i % 11 == 0 && i <= 40))
-        cantidad_exito_critico++
-    else if (i == 100 || (i % 11 == 0 && i > 40))
-        cantidad_fallo_critico++
-    else if (i > 40)
-        cantidad_fallos++
-    else
-        cantidad_exitos++
+    if (i === 1 || (i % 11 === 0 && i <= 40)) {
+        criticalSuccessCount++;
+    } else if (i === 100 || (i % 11 === 0 && i > 40)) {
+        criticalFailCount++;
+    } else if (i > 40) {
+        failCount++;
+    } else {
+        successCount++;
+    }
 }
 
-mostrar_texto("Fallos: " + cantidad_fallos * 1 + "%")
-mostrar_texto("Exitos: " + cantidad_exitos * 1 + "%")
-mostrar_texto("Exito critico: " + cantidad_exito_critico * 1 + "%")
-mostrar_texto("Fallo critico: " + cantidad_fallo_critico * 1 + "%")
+renderProbability("Fails: " + failCount + "%");
+renderProbability("Successes: " + successCount + "%");
+renderProbability("Critical Successes: " + criticalSuccessCount + "%");
+renderProbability("Critical Fails: " + criticalFailCount + "%");
