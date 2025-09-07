d147bd2f883f7de466fe72e3ba3d09287167d4c6
s-ferrari <sol_ferrari20@hotmail.com>
2025-09-06 23:41:32 -0300

    extracted core function into new folder

---
+++ Function/Probability.js
@@ -0,0 +1,7 @@
+// Function to add a new div with the given text to the page
+
+export function renderProbability(text) {
+    const element = document.createElement("div");
+    element.textContent = text;
+    document.body.appendChild(element);
+}
