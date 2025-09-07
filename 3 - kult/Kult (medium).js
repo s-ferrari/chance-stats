9091bcabb682951e2040f8a7eb22df5954f80607
s-ferrari <sol_ferrari20@hotmail.com>
2025-09-06 23:45:45 -0300

    refactor(Kult): improve code structure and import function

--- 3 - kult/Kult (medium).js
+++ 3 - kult/Kult (medium).js
@@ -1,66 +1,24 @@
-const body = document.querySelector("body")
-function mostrar_texto(texto) {
-    const element = document.createElement("div")
-    element.textContent = texto
-    body.appendChild(element)
-}
-// 2 dados de 10 + modificador
-// 15 o mas Exito completo
-// 10-14 Exito parcial
-// 9 o menos Fallo
+import { renderProbability } from "../Function/Probability.js"
 
-let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-let exito_completo = 0
-let exito_parcial = 0
-let fallo = 0
-let modificador = 0
+let fullSuccess = 0;
+let partialSuccess = 0;
+let failure = 0;
+let modifier = 0;
 
-for (let a of numeros) {
-    for (let b of numeros) {
-        let tirada = (a + b) + modificador
-        if (tirada >= 15)
-            exito_completo++
-        else if (tirada >= 10 && tirada <= 14)
-            exito_parcial++
+for (let a of numbers) {
+    for (let b of numbers) {
+        let roll = (a + b) + modifier;
+        if (roll >= 15)
+            fullSuccess++;
+        else if (roll >= 10 && roll <= 14)
+            partialSuccess++;
         else
-            fallo++
+            failure++;
     }
 }
 
-mostrar_texto("Exitos completos: " + exito_completo * 1 + "%")
-mostrar_texto("Exitos parciales: " + exito_parcial * 1 + "%")
-mostrar_texto("Fallos: " + fallo * 1 + "%")
-
-// class ObtenedorDePorcetajes {
-//     exito_completo = 0
-//     exito_parcial = 0
-//     fallo = 0
-//     constructor(modificador) {
-//         for (let a of numeros) {
-//             for (let b of numeros) {
-//                 let tirada = (a + b) + modificador
-//                 if (tirada >= 15)
-//                     this.exito_completo++
-//                 else if (tirada >= 10 && tirada <= 14)
-//                     this.exito_parcial++
-//                 else
-//                     this.fallo++
-//             }
-//         }
-//     }
-// }
-
-// mostrar_texto("")
-
-// let obtenedor_de_porcentajes_con_modificador_0 = new ObtenedorDePorcetajes(0)
-// mostrar_texto("Exitos completos (0): " + obtenedor_de_porcentajes_con_modificador_0.exito_completo * 1 + "%")
-// mostrar_texto("Exitos parciales (0): " + obtenedor_de_porcentajes_con_modificador_0.exito_parcial * 1 + "%")
-// mostrar_texto("Fallos (0): " + obtenedor_de_porcentajes_con_modificador_0.fallo * 1 + "%")
-
-// mostrar_texto("")
-
-// let obtenedor_de_porcentajes_con_modificador_1 = new ObtenedorDePorcetajes(1)
-// mostrar_texto("Exitos completos (1): " + obtenedor_de_porcentajes_con_modificador_1.exito_completo * 1 + "%")
-// mostrar_texto("Exitos parciales (1): " + obtenedor_de_porcentajes_con_modificador_1.exito_parcial * 1 + "%")
-// mostrar_texto("Fallos (1): " + obtenedor_de_porcentajes_con_modificador_1.fallo * 1 + "%")
+renderProbability("Full successes: " + fullSuccess * 1 + "%");
+renderProbability("Partial successes: " + partialSuccess * 1 + "%");
+renderProbability("Failures: " + failure * 1 + "%");
