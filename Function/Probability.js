98df63e194353566d88b7de9f219151c23e617e9
s-ferrari <sol_ferrari20@hotmail.com>
2025-09-10 23:59:29 -0300

    refactor

--- Function/Probability.js
+++ Function/Probability.js
@@ -1,5 +1,4 @@
 // Function to add a new div with the given text to the page
-
 export function renderProbability(text) {
     const element = document.createElement("div");
     element.textContent = text;
